37f46150675d96925c4c38236200e448e2e1718d
oguzhan bulbul <94052888+oguzbulbul@users.noreply.github.com>
2023-03-26 20:34:04 +0300

    Delete prolab2_1_2p directory

--- prolab2_1_2p/run.py
+++
@@ -1,7 +0,0 @@
-from methods_for_maping import *
-menu = create_tkinter_menu()
-menu.mainloop()
-
-# URLS :
-#  http://bilgisayar.kocaeli.edu.tr/prolab2/url1.txt
-#  http://bilgisayar.kocaeli.edu.tr/prolab2/url2.txt
